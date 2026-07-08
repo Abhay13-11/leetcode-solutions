e4f16c9923c8c2886ddc9e7b4b7c538abf64a53c
Abhay Singh Chauhan <singhabhaydpr@gmail.com>
2026-07-08 16:40:29 +0530

    Time: 19 ms (98.45%) | Memory: 152 MB (76.17%) - LeetSync

---
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        const int MOD = 1000000007;
+        int n = s.size();
+
+        vector<long long> pow10(n + 1, 1);
+        for (int i = 1; i <= n; i++)
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+
+        vector<int> sumD(n + 1, 0);
+        vector<int> cntN0(n + 1, 0);
+        vector<long long> p(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            int d = s[i - 1] - '0';
+            sumD[i] = sumD[i - 1] + d;
+            cntN0[i] = cntN0[i - 1] + (d > 0);
+
+            if (d)
+                p[i] = (p[i - 1] * 10 + d) % MOD;
+            else
+                p[i] = p[i - 1];
+        }
+
+        vector<int> ans;
+
+        for (auto &q : queries) {
+            int l = q[0];
+            int r = q[1];
+
+            int sum = sumD[r + 1] - sumD[l];
+            int cnt = cntN0[r + 1] - cntN0[l];
+
+            long long x = (p[r + 1] - p[l] * pow10[cnt] % MOD + MOD) % MOD;
+
+            ans.push_back((x * sum) % MOD);
+        }
+
+        return ans;
+    }
+};
