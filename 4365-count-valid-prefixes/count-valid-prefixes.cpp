321d7989aceba04b0ba4adba25ca75f8210ece7c
Abhay Singh Chauhan <singhabhaydpr@gmail.com>
2026-08-02 10:44:42 +0530

    Time: 1 ms (33.33%) | Memory: 9.5 MB (50.00%) - LeetSync

---
+++ 4365-count-valid-prefixes/count-valid-prefixes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int ans=0;
+        long long c0=0;
+        long long c1=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='1') c1++;
+            else c0++;
+            if(abs(c0-c1)<=1 && abs(c1-c0)>=-1) ans++;
+        }
+        return ans;
+        }
+};
