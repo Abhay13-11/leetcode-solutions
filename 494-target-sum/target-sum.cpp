6b136fc3aee60203f8c4e788fb812cb0307425b7
Abhay Singh Chauhan <singhabhaydpr@gmail.com>
2026-08-19 19:37:34 +0530

    Time: 32 ms (38.27%) | Memory: 15.5 MB (50.60%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,29 @@
 class Solution {
-    int solve(int ind,int count,vector<int> &nums,int target)
+    int countt(int ind,int count,vector<int> &arr,int target,vector<vector<int>> &dp)
     {
-        if(ind==-1 ) return target==0;
-        int add=solve(ind-1,count,nums,target+nums[ind]);
+        if(ind==0)
+        {
+            if(arr[0]==0 && target==0) return 2;
+            if(target==0 || arr[0]==target) return 1;
+            return 0;
+        }
+        if(dp[ind][target]!=0) return dp[ind][target];
+        int notake=countt(ind-1,count,arr,target,dp);
+        int take=0;
+        if(arr[ind]<=target) take= countt(ind-1,count,arr,target-arr[ind],dp);
         
-        int sub=solve(ind-1,count,nums,target-nums[ind]);
-
-        return add+sub;
-        
+        return dp[ind][target]=take+notake;
     }
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-       
-        return solve(n-1,0,nums,target);
+    int findTargetSumWays(vector<int>& arr, int diff) {
+        int sum=0;
+        for(int it : arr) sum+=it;
+        int d=sum-diff;
+        if(d<0 || d%2==1) return 0;
+        d=d/2;
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int>(d+1,0));
+        
+        return countt(n-1,0,arr,d,dp);
     }
 };
