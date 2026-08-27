e4bf65a2787f3719867dfd9f2c519fb6ab4ae0a6
Abhay Singh Chauhan <singhabhaydpr@gmail.com>
2026-08-27 23:51:34 +0530

    Time: 47 ms (60.50%) | Memory: 10.8 MB (81.88%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -22,25 +22,28 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+       
+        vector<int> prev(m+1,0),curr(m+1,0);
         
-        dp[0][0]=true;
+        prev[0]=true;
         for(int j=1;j<=m;j++)
         {
-            if(p[j-1]=='*') dp[0][j]=dp[0][j-1];
+            if(p[j-1]=='*') prev[j]=prev[j-1];
         }
+        
 
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(p[j-1]=='?' || p[j-1]==s[i-1]) dp[i][j]=dp[i-1][j-1];
-               else if(p[j-1]!=s[i-1] && p[j-1]!='*') dp[i][j]=0;
+                if(p[j-1]=='?' || p[j-1]==s[i-1]) curr[j]=prev[j-1];
+               else if(p[j-1]!=s[i-1] && p[j-1]!='*') curr[j]=0;
 
         
-            else dp[i][j]=dp[i-1][j] || dp[i][j-1];
+            else curr[j]=prev[j] || curr[j-1];
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
