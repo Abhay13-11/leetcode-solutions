291059abcea0e5569b9a514d45d1d760c4e94a6b
Abhay Singh Chauhan <singhabhaydpr@gmail.com>
2026-08-30 13:39:08 +0530

    Time: 444 ms (29.92%) | Memory: 211.4 MB (46.84%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,20 @@
 class Solution {
-    int solve(int count,int n,int i,bool buy,vector<int> &prices,vector<vector<vector<int>>> &dp)
-    {
-        if(i==n || count>=2) return 0;
-        if(dp[i][buy][count]!=-1) return dp[i][buy][count];
-        if(!buy) return dp[i][buy][count]=max(solve(count,n,i+1,false,prices,dp),-prices[i]+solve(count,n,i+1,true,prices,dp));
+    // int solve(int count,int n,int i,bool buy,vector<int> &prices,vector<vector<vector<int>>> &dp)
+    // {
+    //     if(i==n || count>=2) return 0;
+    //     if(dp[i][buy][count]!=-1) return dp[i][buy][count];
+    //     if(!buy) return dp[i][buy][count]=max(solve(count,n,i+1,false,prices,dp),-prices[i]+solve(count,n,i+1,true,prices,dp));
 
-        int nosell=solve(count,n,i+1,true,prices,dp);
-        int sell=-1e9;
-        if(count<=1) sell= prices[i]+solve(count+1,n,i+1,false,prices,dp);
-       return dp[i][buy][count]=max(sell,nosell);
-    }
+    //     int nosell=solve(count,n,i+1,true,prices,dp);
+    //     int sell=-1e9;
+    //     if(count<=1) sell= prices[i]+solve(count+1,n,i+1,false,prices,dp);
+    //    return dp[i][buy][count]=max(sell,nosell);
+    // }
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        vector<vector<int>> prev(2,vector<int>(2,-1)),curr(2,vector<int>(2,-1));
+        vector<vector<int>> prev(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
 
         
         for(int i=n-1;i>=0;i--)
@@ -23,19 +23,21 @@ public:
             {
                 for(int count=1;count>=0;count--)
                 {
-                    if(!buy)  dp[i][buy][count]= max(dp[i+1][false][count],-prices[i]+dp[i+1][true][count]);
+                    if(!buy)  curr[buy][count]= max(prev[false][count],-prices[i]+prev[true][count]);
 
                     else
                     {
-                            int nosell=dp[i+1][true][count];
-                            int sell= prices[i]+dp[i+1][false][count+1];
-                             dp[i][buy][count]=max(sell,nosell);
+                            int nosell=prev[true][count];
+                            int sell= prices[i]+prev[false][count+1];
+                             curr[buy][count]=max(sell,nosell);
                     }
                     
                 }
+             
             }
+            prev=curr;
         }
-        return dp[0][0][0];
+        return prev[0][0];
         
     }
 };
