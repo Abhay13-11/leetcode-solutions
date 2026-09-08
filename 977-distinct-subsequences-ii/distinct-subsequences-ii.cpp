85e3499ae537e1132fa079084afafd0eecf25203
Abhay Singh Chauhan <singhabhaydpr@gmail.com>
2026-09-08 11:47:43 +0530

    Time: 2 ms (44.58%) | Memory: 10 MB (13.02%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,23 +1,27 @@
 class Solution {
+    int solve(vector<int> &prev,int n,vector<int> &dp,int mod)
+    {
+        if(n < 0) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
+        int total=(2LL*solve(prev,n-1,dp,mod))%mod;
+        
+        if(prev[n]!=-1) total-=solve(prev,prev[n]-1,dp,mod)%mod;
+        return dp[n]=(total+mod)%mod;
+    }
 public:
     int distinctSubseqII(string s) {
-        int n = s.size();
-        const int MOD = 1e9 + 7;
-        vector<long long> dp(n + 1, 0);
-        vector<int> last(26, -1);
-
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = (2 * dp[i - 1]) % MOD;
-            int ch = s[i - 1] - 'a';
-
-            if (last[ch] != -1) {
-                dp[i] = (dp[i] -dp[last[ch] - 1] + MOD) %MOD;
-            }
-
-            last[ch] = i;
+        int mod=1e9+7;
+        int n=s.size();
+        vector<int> dp(n,-1);
+         vector<int> lastind(26,-1);
+         vector<int> prev(n,-1);
+        for(int i=0;i<n;i++)
+        {
+              prev[i]=lastind[s[i]-'a'];
+            lastind[s[i]-'a']=i;
+          
         }
-
-        return (dp[n] - 1 + MOD) % MOD; // remove empty subsequence
+        return (solve(prev,n-1,dp,mod)-1+mod)%mod;
     }
 };
